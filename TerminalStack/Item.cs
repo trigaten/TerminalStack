af6dd539d9b1f5af563f5db5acf330836d73f481
trigaten <sanderman5@hotmail.com>
2021-05-16 19:06:06 -0400

    init

---
+++ TerminalStack/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TerminalStack
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public TimeSpan TimeExpected { get; set; }
+        public float Priority { get; set; }
+
+        public Item(string name, string description, TimeSpan timeExpected, float priority)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.TimeExpected = timeExpected;
+            this.Priority = priority;
+        }
+    }
+}
---
+++ TerminalStack/PriorityStack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace TerminalStack
+{
+    /// <summary>
+    /// Stores elements ordered by a float priority.
+    /// Multiple elements with the same priority can be stored.
+    /// </summary>
+    public class PriorityStack
+    {
+        public PriorityStack()
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// A node for the PriorityStack
+    /// </summary>
+    /// <remarks>
+    /// stores an element and points to next element
+    /// </remarks>
+    class PriorityNode
+    {
+        public Item ThisItem { get; set; }
+        public Item NextItem { get; set; }
+
+        public PriorityNode(Item item)
+        {
+            this.ThisItem = item;
+            this.NextItem = null;
+        }
+    }
+
+}
